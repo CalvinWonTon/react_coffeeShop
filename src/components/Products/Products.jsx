ed57d5fcc804e0b9303c972963f2c4b95d7b2a32
Calvin Ton <cton@hawk.iit.edu>
2021-11-16 22:31:45 -0600

    made name changes for files

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './productsStyles';
 
 const products = [
   { id: 1, name: 'Espresso', description: 'Espresso shots', price: '$3', image: 'https://globalassets.starbucks.com/assets/ec519dd5642c41629194192cce582135.jpg?impolicy=1by1_wide_topcrop_630'},
@@ -12,8 +13,11 @@ const products = [
 ]
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar}/>
     <Grid container justify='center' spacing={4}>
       {products.map((product) => (
         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>

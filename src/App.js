ad5d81053895fb7801b7bd69218569cb3f4d7fa9
Calvin Ton <cton@hawk.iit.edu>
2021-11-17 02:13:56 -0600

    added cart functionality and routes from product page to cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react'
-import { Products, Navbar } from './components';
+import React, { useState, useEffect } from 'react'
+import { commerce } from './lib/drinks';
+import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchDrinks = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  }
+
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve();
+    setCart(cart)
+  }
+
+  const handleAddToCart = async(productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart)
+  }
+
+  useEffect(() => {
+    fetchDrinks();
+    fetchCart();
+  }, []);
+
+  console.log(cart);
+
   return (
-    <div>
-      <Navbar />
-      <Products/>
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items}/>
+        <Routes>
+          <Route path='/' element={<Products products={products} onAddToCart={handleAddToCart}/>}/>
+          <Route path='/cart' element={<Cart cart={cart}/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App

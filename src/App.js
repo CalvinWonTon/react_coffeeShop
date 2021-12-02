05e9d8c214f1bdcc3c6a153265c5343f9f6b066d
Calvin Ton <cton@hawk.iit.edu>
2021-12-01 19:46:59 -0600

    added handle functions as well as cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { commerce } from './lib/drinks';
-import { Products, Navbar, Cart } from './components';
+import { commerce } from './lib/commerce';
+import { Products, Navbar, Cart, Checkout } from './components';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
@@ -15,12 +15,27 @@ const App = () => {
 
   const fetchCart = async () => {
     const cart = await commerce.cart.retrieve();
-    setCart(cart)
+    setCart(cart);
   }
 
   const handleAddToCart = async(productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart)
+    const { cart } = await commerce.cart.add(productId, quantity);
+    setCart(cart);
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, {quantity});
+    setCart(cart);
+  } 
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
   }
 
   useEffect(() => {
@@ -35,8 +50,9 @@ const App = () => {
       <div>
         <Navbar totalItems={cart.total_items}/>
         <Routes>
-          <Route path='/' element={<Products products={products} onAddToCart={handleAddToCart}/>}/>
-          <Route path='/cart' element={<Cart cart={cart}/>}/>
+          <Route path='/' element={<Products products={products} onAddToCart={handleAddToCart} />} />
+          <Route path='/cart' element={<Cart cart={cart} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} handleEmptyCart={handleEmptyCart} />} />
+          <Route path='/checkout' element={<Checkout cart={cart} />} />
         </Routes>
       </div>
     </Router>

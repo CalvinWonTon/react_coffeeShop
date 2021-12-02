c4fed11a26f5909d41205ece0dd8ad9983036ab7
Calvin Ton <cton@hawk.iit.edu>
2021-12-01 19:47:56 -0600

    commented out tax and total

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import useStyles from './cartStyles';
 import CartItem from './CartItem/CartItem';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
   const classes = useStyles();
 
   const EmptyCart = () => (
-    <Typography variant='subtitle1'>You have no items in your shopping cart
-      <Link to='/' className={classes.link}>start adding some</Link>!
+    <Typography variant='subtitle1'>You have no items in your shopping cart,
+      <Link to='/' className={classes.link}> start adding some</Link>!
     </Typography>
   );
 
@@ -18,15 +18,17 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item}/>
+            <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} />
           </Grid>
         ))}
       </Grid>
       <div className={classes.cardDetails}>
-        <Typography variant='h4'>Subtotal: { cart.subtotal.formatted_with_symbol }</Typography>
+        <Typography variant='h5'>Subtotal: { cart.subtotal.formatted_with_symbol }</Typography>
+        {/* <Typography variant='h5'>Tax: ${ (Math.round(cart.subtotal.formatted * 0.1025 * 100) / 100).toFixed(2) }</Typography>
+        <Typography variant='h5'>Total: ${(Math.round(cart.subtotal.formatted * 1.1025 * 100) / 100).toFixed(2)}</Typography> */}
         <div>
-          <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
-          <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
+          <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary' onClick={handleEmptyCart}>Empty Cart</Button>
+          <Button component={Link} to='/checkout' className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
         </div>
       </div>
     </>
